e9853db8c31936ca504fb1800f4b394cf7a4d6f4
James Weber <joweber123@gmail.com>
2017-06-01 12:20:11 +0900

    cleaned up javascript

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,37 @@
+$(function() {
+  homepagewordfadein();
+  arrowfade();
+});
 
-      $(".frontcontainer1").hover(function(){
-          $('.words1').fadeIn(500);
-      },function(){
-          $('.words1').fadeOut(500);
-      });
+function homepagewordfadein(){
+  $(".frontcontainer1").hover(function(){
+      $('.words1').fadeIn(500);
+  },function(){
+      $('.words1').fadeOut(500);
+  });
 
-      $(".frontcontainer2").hover(function(){
-          $('.words2').fadeIn(500);
-      },function(){
-          $('.words2').fadeOut(500);
-      });
+  $(".frontcontainer2").hover(function(){
+      $('.words2').fadeIn(500);
+  },function(){
+      $('.words2').fadeOut(500);
+  });
+}
 
-        //do code for less than 480px wide
-
-$(document).ready(function(){
+function arrowfade(){
   $('.arrowword').fadeIn(5000);
-});
-
-$(document).ready(function(){
   $('.arrow').fadeIn(5000);
-});
-
-$(document).ready(function() {
-$('.linecontainer').delay(3000).fadeIn(100);
-});
-
-
-
+  $('.linecontainer').delay(3000).fadeIn(100);
+}
 
 $(window).scroll(function() {
-      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop < 30 ) {
-            $('.arrowword' ).stop(true, true).fadeIn(3000);
-      }
-      else {
-            $('.arrowword').stop(true, true).fadeOut(3000);
-      }
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop < 30 ) {
+        $('.arrowword' ).stop(true, true).fadeIn(3000);
+  }
+  else {
+        $('.arrowword').stop(true, true).fadeOut(3000);
+  }
 });
-
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function(){
     Typed.new("#typed", {
@@ -49,8 +39,7 @@ document.addEventListener('DOMContentLoaded', function(){
         typeSpeed: 30,
         backDelay: 2000,
         loop: true,
-        contentType: 'html', // or text
-        // defaults to null for infinite loop
+        contentType: 'html',
         loopCount: null,
     });
 });
